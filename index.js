294d65cb66b5c21e16a9ed2bc6f923d38b3c2c66
Kyla Palos <kyla.palos@gmail.com>
2018-08-29 18:43:42 +0000

    finished recursion

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if(fromN === toN){
+    return fromN;
+  }
+  return sum(fromN + 1, toN) + fromN;
 }
+
+console.log(sum(3, 7));
 
 module.exports = sum;
